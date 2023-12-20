f86df129d3e67c7125ad0b74732ad2d2c40eaad3
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2023-12-21 01:17:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
   // your code here
+	if (!str.trim()) {
+        return '';
+    }
+
+    // Split the string by spaces
+    const words = str.trim().split(' ');
+
+    // Return the first word
+    return words[0];
+
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+const str = prompt("Enter String:");
+alert(firstWord(str));
